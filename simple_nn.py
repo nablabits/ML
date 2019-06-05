c4dde353335bc8fe7d3ba941b4a87ed8b469de3e
David Reguera <birrick@gmail.com>
2019-06-05 17:48:08 +0200

    Add training class and forward method

--- simple_nn.py
+++ simple_nn.py
@@ -41,3 +41,42 @@ def SetUp(initial):
     return pd.DataFrame(data=data)
 
 
+class Train:
+    """Run the network forward to compute the outputs and the error."""
+
+    def __init__(self, df):
+        """Require the weights df."""
+        self.df = df
+
+    def sigma(self, z):
+        """Compute the sigmoid activation function."""
+        return 1 / (1 + np.exp(-z))
+
+    def E(self, yhat, sigma):
+        """Compute the error function."""
+        return 0.5 * (yhat - sigma)
+
+    def forward(self, df):
+        """Fill the nan values in the df."""
+        # fill the mid layer outputs
+        ms = list()
+        for c, val in enumerate(df.iloc[-1, 0]):
+            z = np.dot(val, df.iloc[-1, 1][c])
+            ms.append(self.sigma(z))
+        df.at[len(df) - 1, 'MS'] = ms
+
+        # Compute final output
+        z = np.dot(df.iloc[-1, 2], df.iloc[-1, 3])
+        df.at[len(df) - 1, 'OS'] = self.sigma(z)
+
+        # And the error
+        yhat, sigma = df.iloc[-1, 5], df.iloc[-1, 4]
+        df.at[len(df) - 1, 'Error'] = self.E(yhat, sigma)
+
+        return df
+
+    def go(self):
+        """Launch the training."""
+        f = self.forward(self.df)
+
+        return f

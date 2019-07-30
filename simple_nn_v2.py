f239e4a1b8c85477f4449ce317590f303be948a5
David Reguera <birrick@gmail.com>
2019-07-30 08:53:12 +0200

    Create new simple neural network

---
+++ simple_nn_v2.py
@@ -0,0 +1,8 @@
+"""
+Create from the scratch a neural network that predicts the outcome for the
+logical OR operation.
+
+This version gets rid of pandas dataframe in the previous one.
+"""
+
+import numpy as np
